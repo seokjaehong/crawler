2c339fada1b6ce8fecc82b350c554300a35a5951
seokjae hong <hsj2334@gmail.com>
2018-01-25 16:20:07 +0900

    HTML에 1~100위가 모두 저장되어있고 JS로 나누어 보여주는 방식, 하나로 파일저장, 이후 클래스명으로 구분해서 가져와야함

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ import utils
 from utils import get_top100_list
 
 if __name__ == '__main__' :
-    result = get_top100_list()
-    # for i in result :
-    #     print(i)
+    result = get_top100_list(True)
--- utils.py
+++ utils.py
@@ -30,55 +30,17 @@ def get_top100_list(refresh_html=False):
     os.makedirs(path_data_dir, exist_ok=True)
 
     # 1~50, 50~100위 웹페이지 주소
-    url_chart_realtime_50 = 'https://www.melon.com/chart/index.htm'
-    url_chart_realtime_100 = 'https://www.melon.com/chart/index.htm#params%5Bidx%5D=51'
+    url_chart_realtime = 'https://www.melon.com/chart/index.htm'
 
-    # 1~50위에 해당하는 웹페이지 HTML을
+    # 1~100위에 해당하는 웹페이지 HTML을
     # data/chart_realtime_50.html 에 저장
     # 'xt'모드와 try-except를 쓸 경우
-    file_path = os.path.join(path_data_dir, 'chart_realtime_50.html')
+    file_path = os.path.join(path_data_dir, 'chart_realtime.html')
     try:
-        with open(file_path, 'xt') as f:
-            response = requests.get(url_chart_realtime_50)
+        file_mode = 'wt' if refresh_html else 'xt'
+        with open(file_path, file_mode) as f:
+            response = requests.get(url_chart_realtime)
             source = response.text
             f.write(source)
     except FileExistsError:
         print(f'"{file_path}" file is already exists!')
-
-    # 51~100위에 해당하는 웹페이지 HTML을
-    # data/chart_realtime_100.html 에 저장
-    # 파일이 있는 경우를 검사 후 로직 실행
-    file_path = os.path.join(path_data_dir, 'chart_realtime_100.html')
-    if not os.path.exists(file_path):
-        response = requests.get(url_chart_realtime_100)
-        source = response.text
-        with open(file_path, 'wt') as f:
-            f.write(source)
-    else:
-        print(f'"{file_path}" file is already exists!')
-
-    # file_path = os.path.join(path_data_dir, 'abc.txt')
-    # print(f'file_path: \n{file_path}')
-
-    # result = []
-    # for tr in soup.find_all('tr', class_='lst50'):
-    #     rank = tr.find('span', class_='rank').text
-    #     title = tr.find('div', class_='rank01').find('a').text
-    #     artist = tr.find('div', class_='rank02').find('a').text
-    #     album = tr.find('div', class_='rank03').find('a').text
-    #     url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
-    #     # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
-    #     # .* -> 임의 문자의 최대 반복
-    #     # \. -> '.' 문자
-    #     # .*?/ -> '/'이 나오기 전까지의 최소 반복
-    #     p = re.compile(r'(.*\..*?)/')
-    #     url_img_cover = re.search(p, url_img_cover).group(1)
-    #
-    #     result.append({
-    #         'rank': rank,
-    #         'title': title,
-    #         'url_img_cover': url_img_cover,
-    #         'artist': artist,
-    #         'album': album,
-    #     })
-    #
